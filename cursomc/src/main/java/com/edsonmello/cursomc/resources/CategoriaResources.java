691420fa27775d9715e5731c5c00675d50b7f230
Edson Mello <edspazz@msn.com>
2018-08-01 14:07:27 -0300

    Criação do domain Categoria e alteração da classe CategoriaResources

--- cursomc/src/main/java/com/edsonmello/cursomc/resources/CategoriaResources.java
+++ cursomc/src/main/java/com/edsonmello/cursomc/resources/CategoriaResources.java
@@ -1,17 +1,30 @@
 package com.edsonmello.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edsonmello.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "REST está funcionando";
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categoriaList = new ArrayList<>();
+		
+		categoriaList.add(cat1);
+		categoriaList.add(cat2);
+		
+		return categoriaList;
 	}
 	
 
